0e906c7d66b9334c77a37b44c53ed2d9d9ca0b5f
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-23 10:15:32 -1000

    passed both tests

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -14,7 +14,7 @@ var Direction = require("./Direction").Direction;
 function MatrixRotator(matrix){
   this.matrix = matrix;
 
-};
+}
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
@@ -32,19 +32,29 @@ MatrixRotator.prototype.rotate = function(direction) {
   }
 
   var len = this.matrix.length - 1;
+  var x, y;
   // must be a valid Direction, see Direction.js
-  if(direction === Direction.CW) {
-    //rotate clockwise
-    for(var x = (matrixHeight - 1); x >= 0; x--) {
+  if(direction === Direction.CCW) {
+    //rotate counterclockwise
+    for(x = (matrixHeight - 1); x >= 0; x--) {
       len = (this.matrix.length - 1) - x;
-      for(var y = (matrixHeight - 1); y >= 0; y--) {
+      for(y = (matrixHeight - 1); y >= 0; y--) {
         tempMatrix[len][y] = this.matrix[y][x];
       }
     }
-    console.log(tempMatrix);
-  this.matrix = tempMatrix;
-  } else if(direction === Direction.CCW) {
-    //rotate counterclockwise
+    this.matrix = tempMatrix;
+    return this.matrix;
+  } else if(direction === Direction.CW) { //this is rotating CCW instead of CW. fix
+    //rotate clockwise
+    for(y = (matrixHeight - 1); y >= 0; y--) {
+      len = (matrixHeight - 1) - y;
+      for(x = 0; x < (matrixHeight); x++) {
+        tempMatrix[x][len] = this.matrix[y][x];
+      }
+    }
+    this.matrix = tempMatrix;
+    return this.matrix;
   }
+
 
 };
